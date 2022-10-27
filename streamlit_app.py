6d5d93a77669f7f818c2a77d341d00c87024e932
XinglingDemo <116840395+xinglingdemo@users.noreply.github.com>
2022-10-27 15:22:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title ('My Parents New Healthy Diner')
 
 streamlit.header ('Breakfast Menu')
 streamlit.text ('Omega 3 & Blueberry Oatmeal')
-steamlit.text ('Kale, Spinach & Rocket Smoothie')
+streamlit.text ('Kale, Spinach & Rocket Smoothie')
 streamlit.text ('Hard-Boiled Free-Range Egg')
 
