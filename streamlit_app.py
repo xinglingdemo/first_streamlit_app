52c2b874d30e8f2221acccd780303aca7593d3a7
XinglingDemo <116840395+xinglingdemo@users.noreply.github.com>
2022-10-27 15:27:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ streamlit.text ('🍞 Omega 3 & Blueberry Oatmeal')
 streamlit.text ('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text ('🐔 Hard-Boiled Free-Range Egg')
 
+streamlit.title ('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
